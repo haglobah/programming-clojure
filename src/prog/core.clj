cc1894ce821bf07758d35c5ef61d1971ef6d5374
Beat Hagenlocher <bah@posteo.de>
2024-05-17 14:24:06 +0200

    Add ellipsize function

--- src/prog/core.clj
+++ src/prog/core.clj
@@ -3,6 +3,10 @@
     [clojure.string :as string])
   (:gen-class))
 
+(defn ellipsize [words]
+  (let [[w u v :as the-words] (string/split words #"\s+")]
+    (string/join " " [w u v "... (" (str (count the-words)) "words)"])))
+
 (defn -main
   [& args]
   (println (str "Hello from " (string/upper-case "clojure!"))))
