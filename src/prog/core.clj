6f08af6307b28b82ec1f6968e2ad0a1e46f8ef1d
Beat Hagenlocher <bah@posteo.de>
2024-05-17 15:39:56 +0200

    Add more basic stuff – but then switched to obsidian for documenting

--- src/prog/core.clj
+++ src/prog/core.clj
@@ -7,6 +7,19 @@
   (let [[w u v :as the-words] (string/split words #"\s+")]
     (string/join " " [w u v "... (" (str (count the-words)) "words)"])))
 
-(defn -main
-  [& args]
-  (println (str "Hello from " (string/upper-case "clojure!"))))
+(defn countdown [result x]
+  (if (zero? x)
+    result
+    (countdown (conj result x) (dec x))))
+
+(defn indexed [coll] (map-indexed vector coll))
+
+(defn index-filter [pred coll]
+  (when pred
+    (for [[idx el] (indexed coll) :when (pred el)] idx)))
+
+(defn index-of-any [pred coll]
+  (first (index-filter pred coll)))
+
+(defn -main [& _args]
+  (println (countdown [] 10)))
