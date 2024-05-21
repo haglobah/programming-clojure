b587cef89676a116149c7f83d6fe078c3ad4ba3a
Beat Hagenlocher <bah@posteo.de>
2024-05-21 17:27:17 +0200

    Add clojure-loc

--- src/prog/core.clj
+++ src/prog/core.clj
@@ -1,6 +1,8 @@
 (ns prog.core
   (:require
-    [clojure.string :as string])
+   [clojure.string :as string]) 
+  (:use
+   [clojure.java.io :only (reader)])
   (:gen-class))
 
 (defn ellipsize [words]
@@ -20,6 +22,23 @@
 
 (defn index-of-any [pred coll]
   (first (index-filter pred coll)))
+
+(defn minutes-to-millis [mins] (* mins 1000 60))
+
+(defn recently-modified? [file]
+  (> (.lastModified file)
+     (- (System/currentTimeMillis) (minutes-to-millis 30))))
+
+(defn non-blank? [line] (not (string/blank? line)))
+(defn non-git? [file] (not (.contains (.toString (.getName file)) ".git")))
+(defn clj-source? [file] (.endsWith (.toString (.getName file)) ".clj"))
+
+(defn clojure-loc [base-file]
+  (reduce
+   +
+   (for [file (file-seq base-file) :when (and (non-git? file) (clj-source? file))]
+     (with-open [rdr (reader file)]
+       (count (filter non-blank? (line-seq rdr)))))))
 
 (defn -main [& _args]
   (println (countdown [] 10)))
