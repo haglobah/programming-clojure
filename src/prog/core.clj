02a354e6562144f07680ad05e6cc0eabf2d3f791
Beat Hagenlocher <bah@posteo.de>
2024-05-22 17:34:06 +0200

    Add 4. Functional Programming

--- src/prog/core.clj
+++ src/prog/core.clj
@@ -23,6 +23,8 @@
 (defn index-of-any [pred coll]
   (first (index-filter pred coll)))
 
+; 3. Sequences
+
 (defn minutes-to-millis [mins] (* mins 1000 60))
 
 (defn recently-modified? [file]
@@ -39,6 +41,49 @@
    (for [file (file-seq base-file) :when (and (non-git? file) (clj-source? file))]
      (with-open [rdr (reader file)]
        (count (filter non-blank? (line-seq rdr)))))))
+
+; 4. Functional Programming
+
+(defn lazy-seq-fibo
+  ([] (concat [0 1] (lazy-seq-fibo 0N 1N)))
+  ([a b]
+   (let [n (+ a b)]
+     (lazy-seq
+      (cons n (lazy-seq-fibo b n))))))
+
+(defn fibo []
+  (map first (iterate (fn [[a b ]] [b (+ a b)]) [0N 1N])))
+
+(def lots-of-fibs (take 1000000000 (fibo)))
+
+(comment
+  (rem (nth (lazy-seq-fibo) 1000000) 1000)
+
+  (take 5 (iterate (fn [[a b]] [b (+ a b)]) [0 1]))
+
+  (nth lots-of-fibs 100) 
+  :rcf)
+
+(comment
+  (partition 2 1 [:h :t :t :h :t :t]) 
+
+  :rcf)
+
+(declare my-even? my-odd?)
+
+(defn my-even? [n]
+  (if (= n 0)
+    true
+    #(my-odd? (dec n))))
+
+(defn my-odd? [n]
+  (if (= n 0)
+    false
+    #(my-even? (dec n))))
+
+(comment 
+  (trampoline my-even? 10000000000)
+  :rcf)
 
 (defn -main [& _args]
   (println (countdown [] 10)))
